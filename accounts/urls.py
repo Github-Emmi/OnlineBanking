4b06003d8395bc4daa76b20bc9f632e3acaf4c95
EmmiDev <emma@gmail.com>
2023-10-19 03:58:40 -0500

    sign up page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,10 @@ app_name = 'accounts'
 
 urlpatterns = [
 path('', views.index, name='index'),
-path('user-profile', users.user_profile, name='user_profile')
+path('user-profile', users.user_profile, name='user_profile'),
+       ###########          ###########
+          ##     sign up urls  ##
+       ###########          ###########
+path('enroll-step1', views.enroll_step1, name='enroll_step1'),
+path('enroll-step2', views.enroll_step2, name='enroll_step2'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,11 @@ from .modules import *
 # Create your views here
 def index(request):
      return render(request, 'account_templates/index.html', {})
+
+
+    ##### Sign Up Views
+def enroll_step1(request):
+     return render(request, 'account_templates/online_registration.html', {})
+
+def enroll_step2(request):
+     return render(request, 'account_templates/terms_conditions.html', {})
